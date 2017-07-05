176ab8b2210d020803701ff56feecb97ae2b98fd
emt38 <ezequieltheboss64@gmail.com>
2017-07-05 19:07:10 -0400

    Commit bacano

---
+++ LeccionMVC/LeccionMVC/Controllers/OperacionesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LeccionMVC.Models;
+
+namespace LeccionMVC.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        public ViewResult Accion(string nombre)
+        {
+            return View(model:nombre);
+        }
+
+        // GET: Operaciones
+        public int SumaDeDosNumeros(int? n1, int? n2)
+        {
+            if (!n1.HasValue)
+                n1 = 2;
+            if (!n2.HasValue)
+                n2 = 2;
+
+            return (n1.Value + n2.Value);
+        }
+
+        public DateTime VerFecha()
+        {
+            return DateTime.Now;
+        }
+
+        public string GetMensajeBienvenida(string nombre)
+        {
+            if (string.IsNullOrEmpty(nombre))
+                nombre = "Inserte su nombre en la variable 'nombre'";
+
+            return string.Format("Saludos: {0}", nombre);
+        }
+
+        public Persona VerPersona()
+        {
+            Persona temp = new Persona()
+            {
+                Nombre = "Euxequiel",
+                Apellido = "Monereaux",
+                FechaNacimiento = new DateTime(1996, 7, 28)
+            };
+
+            string personaString = temp.ToString();
+
+            return temp;
+        }
+    }
+}
---
+++ LeccionMVC/LeccionMVC/Models/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeccionMVC.Models
+{
+    public class Persona
+    {
+        public override string ToString()
+        {
+            return string.Format("Nombre: {0}\nApellido: {1}\nFecha de Nacimiento: {2}\nEdad: {3}",
+                                  Nombre, Apellido, FechaNacimiento, Edad);
+        }
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        
+        public int Edad
+        {
+            get
+            {
+                int temp = DateTime.Now.Year - FechaNacimiento.Year - 1;
+
+                if (DateTime.Now.Month > FechaNacimiento.Month
+                  || DateTime.Now.Month == FechaNacimiento.Month && DateTime.Now.Day >= FechaNacimiento.Day)
+                    return temp++;
+
+                return temp;
+            }
+        }
+    }
+
+
+}
